6ded882ce2132b6fd874c8caacc132b701e35f3a
Williamskj <89874192+Williamskj@users.noreply.github.com>
2021-11-15 16:46:25 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+// TODO: Include packages needed for this application
+function init () {
+    const fs = require('fs')
+    const inquirer = require('inquirer')
+    const generate = require('./utils/generateMarkdown.js');
+    const path = require('path');
+    inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "What is a good Title for your project?"
+            },
+            {
+                type: "input",
+                name: "describe",
+                message: "What is a good description for your project/application?"
+            },
+            {
+                type: "input",
+                name: "install",
+                message: "How do you install your application?"
+            },
+            {
+                type: "input",
+                name: "use",
+                message: "How do you use your application?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your email address?",
+
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is your github username?"
+            }
+        ])
+        .then(answer => {
+            fs.writeFileSync(path.join(process.cwd(), 'README.md'), generate(answer)
+            )
+        })
+        .catch(err => console.error(err))
+};
+init()
